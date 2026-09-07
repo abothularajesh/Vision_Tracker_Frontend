6758cd15ec00a77300026b068484302f7ca1d9a0
Abothula Rajesh <abothularajesh58@gmail.com>
2026-09-07 10:50:23 +0530

    Fixed Auth.jsx

--- src/AuthForm.jsx
+++ src/AuthForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./AuthForm.css";
-import api from "./src/api/api.js";
+import api from "./api/api.js";
 
 function AuthForm() {
 
